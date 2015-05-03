80818e6aaac9a08ea8962de09d540bd192516d0e
Allen Yao <alleyao@gmail.com>
2015-05-03 04:37:42 +0000

    Fixed tags endpoint

--- app/controllers/api/tag.js
+++ app/controllers/api/tag.js
@@ -2,9 +2,18 @@ var tagController = require('./../tag');
 
 
 exports.getTags = function(req, res, next) {
-	tagController.getTags(function(tags) {
-		res.json({
-			message: typeof tags
-		});
+	tagController.getTags(function(err, tags) {
+        if (err) {
+            res.status(500).json({
+                message: 'Error',
+                error: err,
+                data: {}
+            });
+        } else {
+            res.status(200).json({
+                message: 'Successfully retrieved tags',
+                data: tags
+            });
+        }
 	});
 };
