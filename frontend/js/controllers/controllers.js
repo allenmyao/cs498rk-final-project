d39d41acb9dd72361ebc946cedd12549047d0096
Allen Yao <alleyao@gmail.com>
2015-05-10 05:13:16 +0000

    Updated stack creation form on homepage

--- frontend/js/controllers/controllers.js
+++ frontend/js/controllers/controllers.js
@@ -20,12 +20,35 @@ appControllers.controller('MainController', [
 
 appControllers.controller('HomepageController', [
     '$scope',
-    function($scope) {
+    'Auth',
+    'Stacks',
+    function($scope, Auth, Stacks) {
+        $scope.bookmark = {
+            name: '',
+            url: '',
+            private: false,
+            tags: []
+        };
         $scope.submitBookmarkForm = function() {
             // Create new stack with name = $scope.name
         };
+
+        $scope.stack = {
+            name: '',
+            color: '',
+            private: false
+        };
         $scope.submitStackForm = function() {
-            // Create new stack with name = $scope.name
+
+            var currentUser = Auth.getUser();
+            var stackData = $scope.stack;
+            stackData.owner_id = currentUser._id;
+            Stacks.create(stackData).success(function(data) {
+                // Successfully created stack
+                console.log('Successfully created stack');
+            }).error(function(data) {
+                // Error creating stack
+            });
         };
     }
 ]);
